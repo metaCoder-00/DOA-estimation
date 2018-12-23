ee3e383f822c561685bcf12c764484374ab73d57
hopesAccount <dengyuhao_123@163.com>
2018-12-23 10:11:59 +0800

    narrowMUSIC

---
+++ IMUSIC.m
@@ -0,0 +1,8 @@
+function theta_hat = IMUSIC(signals, sensors, snapshots, SNR, bandwidth)
+%----@Inupt: signals: signals' vector----%
+%----@       sensors: number of sensors--%
+%----@       snapshots   ----------------%
+%----@       SNR       ------------------%
+%----@       bandwidth: [f_begin, f_end]-%
+    sourceNum = length(signals);
+end
---
+++ MUSIC.m
@@ -0,0 +1,21 @@
+function [theta, spectrum] = MUSIC(data, f, sourceNum, sensorNum)
+%---@Input: data (x = As + n)------%
+%---@       f: frequency (Hz)---%
+%---@       sourceNum: number of sources---%
+%---@       sensorNum: number of sensors---%
+%---@Output: spectrum of MUSIC--%
+    covMat = (data*data')/size(data, 2);    % Covariance matrix
+    [eigenVcts, eigenVals] = eig(covMat);   % Eigen factorization
+    eigenVals = diag(eigenVals);
+    [~, index] = sort(eigenVals);
+    noiseSubspace = eigenVcts(:, index(1: sensorNum - sourceNum));
+    
+    theta = (-90: 0.1:90)';
+    spectrum = zeros(length(theta), 1);
+    c = 3e8;
+    margin = (c/f)/2;                     % Let distance between 2 adjacent sensors equal to wavelength/2    
+    for itr = 1: length(theta)
+        steeringVct = exp(-1j*2*pi*f*(margin*(0: sensorNum - 1)'*sind(theta(itr)))/c);
+        spectrum(itr) = (steeringVct'*steeringVct)/(steeringVct'*(noiseSubspace*noiseSubspace')*steeringVct);
+    end
+end
